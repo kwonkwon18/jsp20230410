ff1a0ac7486bed3a51c8b346c90d22b86936f7ab
kwonkwon <kwonkwon9518@gmail.com>
2023-04-10 16:55:25 +0900

    main

--- src/main/java/com/study/DeleteServlet.java
+++ src/main/java/com/study/DeleteServlet.java
@@ -8,16 +8,16 @@ import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.*;
 
 /**
- * Servlet implementation class ListServlet
+ * Servlet implementation class DeleteServlet
  */
-@WebServlet("/list")
-public class ListServlet extends HttpServlet {
+@WebServlet("/delete")
+public class DeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ListServlet() {
+    public DeleteServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,33 +26,35 @@ public class ListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 1. request param 
-		
-		// 2. business logic
-		HttpSession session = request.getSession();
-		Object o = session.getAttribute("db");
-		
-		if (o == null) {
-			o = new ArrayList<String>();
-			session.setAttribute("db", o);
-		}
-		
-		// 3. add attribute
-		request.setAttribute("list", o);
-		
-		// 4. forward / redirect
-		String view = "/WEB-INF/views/list.jsp";
-		request.getRequestDispatcher(view).forward(request, response);
-		
-		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// 1
+		int index = Integer.parseInt(request.getParameter("index"));
+		
+		// 2
+		HttpSession session = request.getSession();
+		List<String> list = (List<String>) session.getAttribute("db");
+		list.remove(index);
+		
+		// 3
+		
+		
+		// 4
+		String location = request.getContextPath() + "/list";
+		response.sendRedirect(location);
 	}
 
 }
+
+
+
+
+
+
+
