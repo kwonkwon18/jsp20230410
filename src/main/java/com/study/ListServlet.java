e0cbd2310c6d1709ffae1ad7a9ffee1ce49f2481
kwonkwon <kwonkwon9518@gmail.com>
2023-04-10 11:39:48 +0900

    first

---
+++ src/main/java/com/study/ListServlet.java
@@ -0,0 +1,61 @@
+package com.study;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ListServlet
+ */
+@WebServlet("/list")
+public class ListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 1. request param
+		
+		// 2. business logic
+		HttpSession session = request.getSession();
+		Object o = new ArrayList<String>();
+		session.setAttribute("db", o);
+		
+		
+		
+		// 3. add attribute
+		
+//		request.setAttribute("list", o);
+		request.setAttribute("list", List.of("백호","태웅"));
+		
+		// 4. forward / redirect
+		String view = "/WEB-INF/views/list.jsp";
+		request.getRequestDispatcher(view).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
